8dacd18a8ecf460a03eaaa02d1cee72c631660b3
Kirill Lahtachev <lahtachev@gmail.com>
2018-05-15 00:24:56 +0400

    clone function updated

--- test/util/clone.spec.js
+++ test/util/clone.spec.js
@@ -6,9 +6,22 @@ describe('clone repo when it exists', () => {
   beforeEach(() => removeFolder(tmpPath));
 
   describe('test clone function', () => {
-    it('clone', async () => {
-      const results = await clone(repoUrl, tmpPath);
-      expect(repoUrl).to.equal(results);
+    it('can clone', async () => {
+      try {
+        const results = await clone(repoUrl, tmpPath);
+        expect(repoUrl).to.equal(results);
+      } catch (error) {
+        assert.isNotOk('clone', 'clone failed');
+      }
+    });
+
+    it('catch error in clone', async () => {
+      try {
+        const results = await clone(repoUrl + 'badExt', tmpPath);
+        expect(repoUrl).to.equal(results);
+      } catch (error) {
+        assert.isOk('clone', 'error was caught');
+      }
     });
   });
 });
