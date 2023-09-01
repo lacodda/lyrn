2840eb2e0d46e03a13d63aeba270e5f1966dbfc6
Kirill Lakhtachev <lahtachev@gmail.com>
2023-09-01 17:49:39 +0200

    feat(create): added ui generator for react

--- src/templates/react.rs
+++ src/templates/react.rs
@@ -61,6 +61,13 @@ fn app(_project: &ProjectProps) -> HashMap<String, Content> {
         ("src/components/About.tsx".into(), Content::Str(component_page("About".into()))),
         ("src/components/Info.tsx".into(), Content::Str(component_page("Info".into()))),
         ("src/images/logo.svg".into(), Content::Str(logo("React".into()))),
+        ("src/ui/index.ts".into(), Content::Str(ui_index())),
+        ("src/ui/components/Button.tsx".into(), Content::Str(button())),
+        ("src/ui/components/Navbar.tsx".into(), Content::Str(navbar())),
+        ("src/ui/styles/index.scss".into(), Content::Str(style_index())),
+        ("src/ui/styles/_reset.scss".into(), Content::Str(style_reset())),
+        ("src/ui/styles/_variables.scss".into(), Content::Str(style_variables())),
+        ("src/ui/styles/_scaffolding.scss".into(), Content::Str(style_scaffolding())),
     ])
 }
 
@@ -249,4 +256,239 @@ fn logo(name: String) -> String {
 "###,
         name
     )
+}
+
+fn ui_index() -> String {
+    r###"import './styles/index.scss';
+export * from './components/Button';
+export * from './components/Navbar';
+"###
+    .into()
+}
+
+fn button() -> String {
+    r###"import styled from 'styled-components';
+
+export const Button = styled.button`
+  background-image: linear-gradient(to top, var(--teal) 0%, var(--blue) 51%, var(--teal) 100%);
+  padding: .75rem .5rem;
+  text-align: center;
+  text-transform: none;
+  transition: 0.5s;
+  background-size: auto 200%;
+  color: var(--white);
+  border-radius: 0.5rem;
+  display: flex;
+  border-color: transparent;
+  font-weight: 400;
+  font-size: 0.8rem;
+  cursor: pointer;
+  line-height: 0.5rem;
+  align-items: center;
+  height: min-content;
+  &:hover {
+    background-position: right center;
+    color: var(--white);
+    text-decoration: none;
+  }
+  &.size-l {
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 1.8rem;
+  }
+`;
+
+export default Button;
+"###
+    .into()
+}
+
+fn navbar() -> String {
+    r###"import React from 'react';
+import styled from 'styled-components';
+
+interface NavbarProps {
+  children: React.ReactNode;
+}
+
+const NavbarMenu = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 1rem;
+    gap: 1rem;
+    a {
+      color: var(--white);
+      font-weight: 700;
+      text-decoration: none;
+      transition: 1s;
+      text-transform: uppercase;
+      &:hover {
+        color: var(--purple);
+      }
+    }
+    input, textarea, select {
+      padding: .45rem .5rem;
+      border-color: transparent;
+      border-radius: 0.5rem;
+      font-size: 0.8rem;
+      line-height: 0.5rem;
+      height: min-content;
+      color: var(--black);
+    }
+    input {
+      width: 100%;
+    }
+  `;
+
+const NavbarContainer = styled.div`
+    position: absolute;
+    width: 100%;
+  `;
+
+export const Navbar: React.FC<{ children: React.ReactNode }> = ({ children }: NavbarProps) => (
+  <NavbarContainer>
+    <NavbarMenu>
+      {children}
+    </NavbarMenu>
+  </NavbarContainer>
+);
+
+export default Navbar;
+"###
+    .into()
+}
+
+fn style_index() -> String {
+    r###"@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap');
+
+@import 'reset';
+@import 'variables';
+@import 'scaffolding';
+"###
+    .into()
+}
+
+fn style_reset() -> String {
+    r###"/*
+  1. Use a more-intuitive box-sizing model.
+*/
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+/*
+    2. Remove default margin
+  */
+* {
+  margin: 0;
+}
+
+/*
+    3. Allow percentage-based heights in the application
+  */
+html,
+body {
+  height: 100%;
+}
+
+/*
+    Typographic tweaks!
+    4. Add accessible line-height
+    5. Improve text rendering
+  */
+body {
+  line-height: 1.5;
+  -webkit-font-smoothing: antialiased;
+}
+
+/*
+    6. Improve media defaults
+  */
+img,
+picture,
+video,
+canvas,
+svg {
+  display: block;
+  max-width: 100%;
+}
+
+/*
+    7. Remove built-in form typography styles
+  */
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+
+/*
+    8. Avoid text overflows
+  */
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  overflow-wrap: break-word;
+}
+
+/*
+    9. Create a root stacking context
+  */
+#root,
+#__next {
+  isolation: isolate;
+}
+"###
+    .into()
+}
+
+fn style_variables() -> String {
+    r###":root {
+    --black: #343434;
+    --gray: #dedede;
+    --blue: #06347e;
+    --white: #ffffff;
+    --teal: #008080;
+    --azure: #31CDDD;
+    --fuxia: #F700F7;
+    --pink: #ec62b5;
+    --violet: #9333ea;
+    --purple: #38006b;
+    --lime: #86efac;
+    --gr-teal-blue: linear-gradient(to right, var(--teal) 0%, var(--blue) 100%);
+    --gr-teal-pink: linear-gradient(to right, var(--teal) 0%, var(--pink) 100%);
+    --gr-azure-fuxia: linear-gradient(to right, var(--azure) 0%, var(--fuxia) 100%);
+    --gr-violet-purple: linear-gradient(to right, var(--violet) 0%, var(--purple) 100%);
+    --gr-azure-pink: linear-gradient(var(--azure), var(--pink));
+    --gr-lime-blue: linear-gradient(var(--lime), var(--blue));
+    --gr-pink-violet: linear-gradient(var(--pink), var(--violet));
+    --font-family: 'Noto Sans', sans-serif;
+    --font-size: 20px;
+    --font-size-h1: 2.6rem;
+    --font-size-h2: 1.6rem;
+    --font-size-h3: 1.1rem;
+}
+"###
+    .into()
+}
+
+fn style_scaffolding() -> String {
+    r###"html {
+  font-size: var(--font-size);
+  font-family: var(--font-family);
+  color: var(--black);
+}
+
+#app {
+  display: contents;
+}
+"###
+    .into()
 }
