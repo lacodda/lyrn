f91af1fbad82f0994d3929719d328f34d4bf82c4
Kirill Lakhtachev <lahtachev@gmail.com>
2023-10-04 21:28:51 +0200

    chore: Removed unused functions in helpers

--- src/libs/helpers.rs
+++ src/libs/helpers.rs
@@ -1,48 +1,7 @@
 use super::types::User;
-use serde::{Serialize, Serializer};
 use spinners::{Spinner, Spinners};
-use std::collections::{BTreeMap, HashMap};
 use std::error::Error;
 use std::process::Command;
-
-pub fn _ordered_map<S, K: Ord + Serialize, V: Serialize>(value: &HashMap<K, V>, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    let ordered: BTreeMap<_, _> = value.iter().collect();
-    ordered.serialize(serializer)
-}
-
-pub fn _opt_ordered_map<S, K: Ord + Serialize, V: Serialize>(value: &Option<HashMap<K, V>>, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    let inside = value.as_ref().unwrap();
-    let ordered: BTreeMap<_, _> = inside.iter().collect();
-    ordered.serialize(serializer)
-}
-
-pub fn _to_hash_map(value: &[(&str, &str)]) -> HashMap<String, String> {
-    return value.into_iter().map(|(p, v)| (p.to_string(), v.to_string())).collect::<HashMap<_, _>>();
-}
-
-pub fn _merge_opt_hashmaps<T>(a: Option<HashMap<String, T>>, b: Option<HashMap<String, T>>) -> Option<HashMap<String, T>> {
-    if a.is_some() && b.is_some() {
-        let mut base = a.unwrap();
-        base.extend(b.unwrap().into_iter());
-        return Some(base);
-    }
-    b.or(a)
-}
-
-pub fn _merge_opt_vectors<T>(a: Option<Vec<T>>, b: Option<Vec<T>>) -> Option<Vec<T>> {
-    if a.is_some() && b.is_some() {
-        let mut base = a.unwrap();
-        base.extend(b.unwrap());
-        return Some(base);
-    }
-    b.or(a)
-}
 
 pub fn get_git_user() -> Result<User, Box<dyn Error>> {
     let user_name = Command::new("git").args(["config", "user.name"]).output()?;
